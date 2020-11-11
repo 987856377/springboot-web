a82254797d7cc681c4da9ee06466fc1a68663e52
XuZhenkui <xuzhenk@bsoft.com.cn>
2020-11-11 17:33:11 +0800

    添加: 获取post请求中json数据 和 返回json格式的数据

--- src/main/java/com/spring/web/servlet/BasicServlet.java
+++ src/main/java/com/spring/web/servlet/BasicServlet.java
@@ -1,11 +1,15 @@
 package com.spring.web.servlet;
 
+import com.alibaba.fastjson.JSONObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * @Description
@@ -23,6 +27,22 @@ public class BasicServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().print("Welcome to Basic master branch Servlet");
+        // 获取 post 请求中 json 数据
+        String param = null;
+        BufferedReader streamReader = new BufferedReader(new InputStreamReader(req.getInputStream(), "UTF-8"));
+        StringBuilder responseStrBuilder = new StringBuilder();
+        String inputStr;
+        while ((inputStr = streamReader.readLine()) != null) {
+            responseStrBuilder.append(inputStr);
+        }
+
+        if(responseStrBuilder.length() > 0){
+            JSONObject jsonObject = JSONObject.parseObject(responseStrBuilder.toString());
+            param = jsonObject.toJSONString();
+        }
+
+        // 返回 json 格式的数据
+        resp.setContentType("application/json; charset=UTF-8");
+        resp.getWriter().write(param);
     }
 }
